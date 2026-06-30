054771a8545bf51abcd67e730e0a898ffc79dfea
nehasree25 <nehasreemendu@gmail.com>
2026-06-30 23:00:13 +0530

    added face detection module

---
+++ faceDetectionModule.py
@@ -0,0 +1,57 @@
+import cv2
+import mediapipe as mp
+import time
+
+class FaceDetector:
+    def __init__(self, min_detection_confidence=0.5):
+        self.min_detection_confidence = min_detection_confidence
+        self.mp_face_detection = mp.solutions.face_detection
+        self.face_detection = self.mp_face_detection.FaceDetection(min_detection_confidence=self.min_detection_confidence)
+        self.mp_draw = mp.solutions.drawing_utils
+
+    def detect_faces(self, img, draw=True):
+        rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = self.face_detection.process(rgb_img)
+
+        if results.detections:
+            for id, detection in enumerate(results.detections):
+                if draw:
+                    self.mp_draw.draw_detection(img, detection)
+                    cv2.putText(
+                        img,
+                        f'Face Detected',
+                        (20, 100),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1,
+                        (255, 255, 0),
+                        2
+                    )
+        return img
+def main():
+    cap = cv2.VideoCapture("videos/video1.mp4")
+    detector = FaceDetector()
+    pTime = time.time()
+
+    while True:
+        success, img = cap.read()
+        if not success:
+            break
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, f'FPS: {int(fps)}', (20, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1,
+                    (255, 255, 0), 2)
+
+        img = detector.detect_faces(img)
+
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
